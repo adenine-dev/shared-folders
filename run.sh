07e1f0cf6e1c539519cb74fc890b0d2529a5d07d
ztco224 <ztc0611@gmail.com>
2021-04-15 21:53:39 -0400

    Fixed Mac Build System

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-terminal -e python server.py
-terminal -e python client.py
+osascript -e 'tell app "Terminal"
+    do script "cd /Users/ztc0611/Documents/GitHub/shared-folders && python3 server.py"
+    do script "cd /Users/ztc0611/Documents/GitHub/shared-folders && python3 client.py"
+end tell'
